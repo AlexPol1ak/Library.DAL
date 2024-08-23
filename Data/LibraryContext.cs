6ab5003ce2983e17e3b3c31acffa70bad031c11e
Alex <99472724+AlexPol1ak@users.noreply.github.com>
2024-08-24 02:30:16 +0300

    Added a database context class. Added static class with prototype of configuration methods for tables. Added prototypes of repository classes for tables.

---
+++ Data/LibraryContext.cs
@@ -0,0 +1,53 @@
+﻿using Library.Domain.Entities.Books;
+using Library.Domain.Entities.Users;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace Library.DAL.Data
+{
+    public class LibraryContext: DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Stuff>  Stuff{ get; set; } = null!;
+        public DbSet<Request> Requests { get; set; } = null!;
+        public DbSet<Author> Authors { get; set; } = null!;
+        public DbSet<Book> Books { get; set; } = null!;
+        public DbSet<BookHistory> BookHistories { get; set; } = null!;
+        public DbSet<Genre> Genres { get; set; } = null!;
+        public DbSet<Rack> Racks { get; set; } = null!;
+        public DbSet<Term> Terms { get; set; } = null!;
+
+        public LibraryContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+
+        /// <summary>
+        /// Выполняет конфигруацию таблиц.
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<User>(ModelsConfig.UserConfig);
+            modelBuilder.Entity<Stuff>(ModelsConfig.StuffConfig);
+            modelBuilder.Entity<Request>(ModelsConfig.RequestConfig);
+            modelBuilder.Entity<Author>(ModelsConfig.AuthorConfig);
+            modelBuilder.Entity<Book>(ModelsConfig.BookConfig);
+            modelBuilder.Entity<BookHistory>(ModelsConfig.BookHistoryConfig);
+            modelBuilder.Entity<Genre>(ModelsConfig.GenreConfig);
+            modelBuilder.Entity<Rack>(ModelsConfig.RackConfig);
+            modelBuilder.Entity<Term>(ModelsConfig.TermConfig);
+        }
+    }
+}
---
+++ Data/ModelsConfig.cs
@@ -0,0 +1,71 @@
+﻿using Library.Domain.Entities.Books;
+using Library.Domain.Entities.Users;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.DAL.Data
+{
+    /// <summary>
+    /// Класс конфигурации моделей.
+    /// </summary>
+    public static class ModelsConfig
+    {
+        // Конифигурация таблицы читателя.
+        static public void UserConfig(EntityTypeBuilder<User> builder)
+        {
+
+        }
+
+        // Конифигурация таблицы персонала.
+        static public void StuffConfig(EntityTypeBuilder<Stuff> builder)
+        {
+
+        }
+
+        // Конифигурация таблицы заявок на книгу.
+        static public void RequestConfig(EntityTypeBuilder<Request> builder)
+        {
+
+        }
+
+        // Конифигурация таблицы авторов книг.
+        static public void AuthorConfig(EntityTypeBuilder<Author> builder)
+        {
+
+        }
+
+        // Конифигурация таблицы книг.
+        static public void BookConfig(EntityTypeBuilder<Book> builder)
+        {
+
+        }
+
+        // Конифигурация таблицы историй книги.
+        static public void BookHistoryConfig(EntityTypeBuilder<BookHistory> builder)
+        {
+
+        }
+
+        // Конифигурация таблицы жанров.
+        static public void GenreConfig(EntityTypeBuilder<Genre> builder)
+        {
+
+        }
+
+        // Конифигурация таблицы стелажей.
+        static public void RackConfig(EntityTypeBuilder<Rack> builder)
+        {
+
+        }
+
+        // Конифигурация таблицы правил выдачи книг.
+        static public void TermConfig(EntityTypeBuilder<Term> builder)
+        {
+
+        }
+    }
+}
